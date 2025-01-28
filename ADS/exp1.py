29ba0b679b568841c110f70e1c1c167f2174605d
Vighnesh Adelkar <vighnesh.adelkar21@gmail.com>
2025-01-28 12:44:30 +0530

    Update exp1.py

--- ADS/exp1.py
+++ ADS/exp1.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+data = pd.read_csv(r"C:\Users\ASUS\Desktop\vighnesh\dwm\Google.csv")
+
+print("First five rows of the dataset:")
+print(data.head())
+
+print("\nDescriptive Statistics:")
+print(data.describe(include='all'))
+
+print("\nMissing Values in Each Column:")
+print(data.isnull().sum())
